9774002b6dfe88a5b975389238e007f184af7dea
pi3rrot <4dakto@gmail.com>
2018-04-08 11:22:02 +0900

    UsingTypeInference

---
+++ UsingTypeInference.ts
@@ -0,0 +1,9 @@
+// UsingTypeInference
+
+let userName;
+userName = ['Siver', 'Surfer'];
+
+let userId = (a: string, b: number) => a + b;
+
+let target = document.getElementById('target');
+target.onclick = (event) => event.button;
